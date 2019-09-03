6b4b2035f82aee5913b4353182b98bfc03644e60
Peter Peill <peter.peill@yahoo.com>
2019-09-03 11:04:55 +0300

    task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,50 @@
 class TasksController < ApplicationController
+  def index
+    # GET /task
+    @tasks = Task.all
+
+  end
+
+  def show
+    # GET /task/:id
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    # GET /task/new
+    @task = Task.new
+  end
+
+  def create
+    # POST /task
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to tasks_path
+  end
+
+  def edit
+    # GET /task/:id/edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    # PATCH /task/:id
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to tasks_path
+
+  end
+
+  def destroy
+    # DELETE /task/:id
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,15 @@ Rails.application.routes.draw do
   # index
   get 'tasks', to: 'tasks#index'
   # create
-  post 'tasks', to: 'tasks#create'
+  post 'tasks', to: 'tasks#create', as: 'create'
   # new
-  get 'tasks/new', to: 'tasks#new'
+  get 'tasks/new', to: 'tasks#new', as: 'new'
   # edit
-  get 'tasks/:id/edit', to: 'tasks#edit'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit'
   # show
-  get 'tasks/:id', to: 'tasks#show'
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
   # update
-  patch 'restaurants/:id', to: 'restaurants#update'
+  patch 'tasks/:id', to: 'tasks#update'
   # destroy
   delete 'tasks/:id', to: 'tasks#destroy'
 end
